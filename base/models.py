9d586c6215a06edbda7040236c139689db5e8d57
Mukul Chugh <mukul@zenduty.com>
2022-08-28 05:29:52 +0530

    User login functionality

--- base/models.py
+++ base/models.py
@@ -7,6 +7,7 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
+        
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
--- base/views.py
+++ base/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.contrib.auth import login, authenticate, logout
+from .models import Room, Topic
 from .forms import RoomForm
 
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User not found.')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials.')
+            return redirect('login')
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+            
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
+    topic = Topic.objects.all()
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topic': topic, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request,pk):
