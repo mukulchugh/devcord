23736a72adb592a791a893d900292300143fad68
Mukul Chugh <mukul@zenduty.com>
2022-08-28 22:18:29 +0530

    Minor Fixes

--- devcord/settings.py
+++ devcord/settings.py
@@ -124,7 +124,6 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
-# STATIC_ROOT =
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
