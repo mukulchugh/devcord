0f495076c7cbc7b82977c976cd2afeb708227892
Mukul Chugh <mukul@zenduty.com>
2022-08-28 19:54:51 +0530

    Minor Fixes

--- devcord/settings.py
+++ devcord/settings.py
@@ -108,6 +108,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# STATIC_ROOT = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
