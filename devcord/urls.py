9d0b82e4485ad0a7ed2d66403bef316ac0b1ff89
Mukul Chugh <mukulchughofficial@gmail.com>
2022-08-29 19:29:06 +0530

    Update urls.py

--- devcord/urls.py
+++ devcord/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('api/', include('base.api.urls'))
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
